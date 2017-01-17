54b10fdb7caa2dee1e45e850ee698094ba6b557c
Christian Karlsson <krille.karlsson@gmail.com>
2017-01-17 18:53:08 +0100

    Initial Commit

---
+++ QuizHock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuizHock
+//
+//  Created by Christian Karlsson on 2017-01-17.
+//  Copyright © 2017 Christian Karlsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuizHock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuizHock
+//
+//  Created by Christian Karlsson on 2017-01-17.
+//  Copyright © 2017 Christian Karlsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
