5c21b7601d545c3f1ec58bfd9b578715a9705a66
Christian Karlsson <krille.karlsson@gmail.com>
2017-01-20 16:26:29 +0100

    Added more questions to replace the placeholders

--- QuizHock/QuestionLibrary.m
+++ QuizHock/QuestionLibrary.m
@@ -31,19 +31,19 @@ int const GAMES_PER_SESSION = 5;
         
         Question *q2 = [[Question alloc] initWithQuestion:@"Hur många guld har Luleå HF tagit?" andAnswer:@"1" andFalseAnswers:@[@"2", @"3", @"4"]];
         
-        Question *q3 = [[Question alloc] initWithQuestion:@"Q3?" andAnswer:@"A" andFalseAnswers:@[@"B", @"C", @"D"]];
+        Question *q3 = [[Question alloc] initWithQuestion:@"Hur många minuters utvisning får man för hög klubba som resulterar i blodvite?" andAnswer:@"4" andFalseAnswers:@[@"2", @"5", @"10"]];
         
-        Question *q4 = [[Question alloc] initWithQuestion:@"Q4?" andAnswer:@"A" andFalseAnswers:@[@"B", @"C", @"D"]];
+        Question *q4 = [[Question alloc] initWithQuestion:@"Vilket är det minsta antalet spelare på planen samtidigt?" andAnswer:@"8" andFalseAnswers:@[@"4", @"6", @"10"]];
         
-        Question *q5 = [[Question alloc] initWithQuestion:@"Q5?" andAnswer:@"A" andFalseAnswers:@[@"B", @"C", @"D"]];
+        Question *q5 = [[Question alloc] initWithQuestion:@"Vad heter utvisningen när man slår någon med klubban?" andAnswer:@"Slashing" andFalseAnswers:@[@"Hooking", @"Hitting", @"Roughing"]];
         
-        Question *q6 = [[Question alloc] initWithQuestion:@"Q6?" andAnswer:@"A" andFalseAnswers:@[@"B", @"C", @"D"]];
+        Question *q6 = [[Question alloc] initWithQuestion:@"Vem gjorde mest mål i grundserien 2015/16?" andAnswer:@"Nick Johnson" andFalseAnswers:@[@"Broc Little", @"Richard Gynge", @"Milan Gulas"]];
         
-        Question *q7 = [[Question alloc] initWithQuestion:@"Q7?" andAnswer:@"A" andFalseAnswers:@[@"B", @"C", @"D"]];
+        Question *q7 = [[Question alloc] initWithQuestion:@"Vem vann utvisningsligan 2015/16?" andAnswer:@"Daniel Rahimi" andFalseAnswers:@[@"Ilari Melart", @"Ryan O'Byrne", @"Jere Karalahti"]];
         
-        Question *q8 = [[Question alloc] initWithQuestion:@"Q8?" andAnswer:@"A" andFalseAnswers:@[@"B", @"C", @"D"]];
+        Question *q8 = [[Question alloc] initWithQuestion:@"Vilket lag åkte ur SHL säsongen 2015/16?" andAnswer:@"MODO" andFalseAnswers:@[@"AIK", @"Timrå", @"Mora"]];
         
-        Question *q9 = [[Question alloc] initWithQuestion:@"Q9?" andAnswer:@"A" andFalseAnswers:@[@"B", @"C", @"D"]];
+        Question *q9 = [[Question alloc] initWithQuestion:@"Hur många spelare kan som mest få poäng vid mål?" andAnswer:@"3" andFalseAnswers:@[@"1", @"2", @"4"]];
 
         self.questions = @[q0, q1, q2, q3, q4, q5, q6, q7, q8, q9];
         self.player = [[Player alloc] init];
