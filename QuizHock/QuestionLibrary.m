0c1e2d6e47d25be5d67c1dc5d0063f356a622019
Christian Karlsson <krille.karlsson@gmail.com>
2017-01-21 10:43:19 +0100

    refactoring

--- QuizHock/QuestionLibrary.m
+++ QuizHock/QuestionLibrary.m
@@ -25,28 +25,28 @@ int const GAMES_PER_SESSION = 5;
     self = [super init];
     if (self) {
 
-        self.questions =
-        Question *q0 = [[Question alloc] initWithQuestion:@"Vilket nummer hade HV71's poängkung Johan Davidsson?" andAnswer:@"76" andFalseAnswers:@[@"71", @"21", @"28"]];
+        self.questions = @[
+                           
+        [[Question alloc] initWithQuestion:@"Vilket nummer hade HV71's poängkung Johan Davidsson?" andAnswer:@"76" andFalseAnswers:@[@"71", @"21", @"28"]],
         
-        Question *q1 = [[Question alloc] initWithQuestion:@"Hur många omgångar spelas det i SHL efter övergången till 14 lag?" andAnswer:@"52" andFalseAnswers:@[@"55", @"48", @"50"]];
+        [[Question alloc] initWithQuestion:@"Hur många omgångar spelas det i SHL efter övergången till 14 lag?" andAnswer:@"52" andFalseAnswers:@[@"55", @"48", @"50"]],
         
-        Question *q2 = [[Question alloc] initWithQuestion:@"Hur många guld har Luleå HF tagit?" andAnswer:@"1" andFalseAnswers:@[@"2", @"3", @"4"]];
+        [[Question alloc] initWithQuestion:@"Hur många guld har Luleå HF tagit?" andAnswer:@"1" andFalseAnswers:@[@"2", @"3", @"4"]],
         
-        Question *q3 = [[Question alloc] initWithQuestion:@"Hur många minuters utvisning får man för hög klubba som resulterar i blodvite?" andAnswer:@"4" andFalseAnswers:@[@"2", @"5", @"10"]];
+        [[Question alloc] initWithQuestion:@"Hur många minuters utvisning får man för hög klubba som resulterar i blodvite?" andAnswer:@"4" andFalseAnswers:@[@"2", @"5", @"10"]],
         
-        Question *q4 = [[Question alloc] initWithQuestion:@"Vilket är det minsta antalet spelare på planen samtidigt?" andAnswer:@"8" andFalseAnswers:@[@"4", @"6", @"10"]];
+        [[Question alloc] initWithQuestion:@"Vilket är det minsta antalet spelare på planen samtidigt?" andAnswer:@"8" andFalseAnswers:@[@"4", @"6", @"10"]],
         
-        Question *q5 = [[Question alloc] initWithQuestion:@"Vad heter utvisningen när man slår någon med klubban?" andAnswer:@"Slashing" andFalseAnswers:@[@"Hooking", @"Hitting", @"Roughing"]];
+        [[Question alloc] initWithQuestion:@"Vad heter utvisningen när man slår någon med klubban?" andAnswer:@"Slashing" andFalseAnswers:@[@"Hooking", @"Hitting", @"Roughing"]],
         
-        Question *q6 = [[Question alloc] initWithQuestion:@"Vem gjorde mest mål i grundserien 2015/16?" andAnswer:@"Nick Johnson" andFalseAnswers:@[@"Broc Little", @"Richard Gynge", @"Milan Gulas"]];
+        [[Question alloc] initWithQuestion:@"Vem gjorde mest mål i grundserien 2015/16?" andAnswer:@"Nick Johnson" andFalseAnswers:@[@"Broc Little", @"Richard Gynge", @"Milan Gulas"]],
         
-        Question *q7 = [[Question alloc] initWithQuestion:@"Vem vann utvisningsligan 2015/16?" andAnswer:@"Daniel Rahimi" andFalseAnswers:@[@"Ilari Melart", @"Ryan O'Byrne", @"Jere Karalahti"]];
+        [[Question alloc] initWithQuestion:@"Vem vann utvisningsligan 2015/16?" andAnswer:@"Daniel Rahimi" andFalseAnswers:@[@"Ilari Melart", @"Ryan O'Byrne", @"Jere Karalahti"]],
         
-        Question *q8 = [[Question alloc] initWithQuestion:@"Vilket lag åkte ur SHL säsongen 2015/16?" andAnswer:@"MODO" andFalseAnswers:@[@"AIK", @"Timrå", @"Mora"]];
+        [[Question alloc] initWithQuestion:@"Vilket lag åkte ur SHL säsongen 2015/16?" andAnswer:@"MODO" andFalseAnswers:@[@"AIK", @"Timrå", @"Mora"]],
         
-        Question *q9 = [[Question alloc] initWithQuestion:@"Hur många spelare kan som mest få poäng vid mål?" andAnswer:@"3" andFalseAnswers:@[@"1", @"2", @"4"]];
-
-        //self.questions = @[q0, q1, q2, q3, q4, q5, q6, q7, q8, q9];
+        [[Question alloc] initWithQuestion:@"Hur många spelare kan som mest få poäng vid mål?" andAnswer:@"3" andFalseAnswers:@[@"1", @"2", @"4"]]];
+        
         self.player = [[Player alloc] init];
         self.currentQuestion = [[Question alloc] init];
         self.sessionQuestions = [[NSMutableArray alloc] init];
